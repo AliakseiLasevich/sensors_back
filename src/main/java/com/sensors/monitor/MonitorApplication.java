71dc597df8895f1eeadbc86609f3f64a858d3b54
Aliaksei_Lasevich <alexey.losevich@gmail.com>
2023-11-13 13:31:23 +0100

    Added Exception handler

--- src/main/java/com/sensors/monitor/MonitorApplication.java
+++ src/main/java/com/sensors/monitor/MonitorApplication.java
@@ -22,26 +22,26 @@ public class MonitorApplication {
 
 
     //       Use it for the very first run to generate admin user
-    @Bean
-    public CommandLineRunner commandLineRunner(
-            AuthenticationService service
-    ) {
-        return args -> {
-            var admin = RegisterRequest.builder()
-                    .login("admin")
-                    .password("password")
-                    .roleIds(List.of(1))
-                    .build();
-            System.out.println("Admin token: " + authenticationService.register(admin).getAccessToken().getToken().toString());
-
-
-            var user = RegisterRequest.builder()
-                    .login("user")
-                    .password("password")
-                    .roleIds(List.of(2))
-                    .build();
-            System.out.println("User token: " + authenticationService.register(user).getAccessToken().getToken().toString());
-        };
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner(
+//            AuthenticationService service
+//    ) {
+//        return args -> {
+//            var admin = RegisterRequest.builder()
+//                    .login("admin")
+//                    .password("password")
+//                    .roleIds(List.of(1))
+//                    .build();
+//            System.out.println("Admin token: " + authenticationService.register(admin).getAccessToken().getToken().toString());
+//
+//
+//            var user = RegisterRequest.builder()
+//                    .login("user")
+//                    .password("password")
+//                    .roleIds(List.of(2))
+//                    .build();
+//            System.out.println("User token: " + authenticationService.register(user).getAccessToken().getToken().toString());
+//        };
+//    }
 
 }
