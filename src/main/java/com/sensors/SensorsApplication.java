690fd9bd3032b7526d07ebce4335526f9e7134c9
Aliaksei_Lasevich <alexey.losevich@gmail.com>
2023-11-20 17:28:46 +0100

    small fixes

--- src/main/java/com/sensors/SensorsApplication.java
+++ src/main/java/com/sensors/SensorsApplication.java
@@ -1,9 +1,14 @@
 package com.sensors;
 
+import com.sensors.dto.request.RegisterRequest;
 import com.sensors.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 @RequiredArgsConstructor
--- src/main/java/com/sensors/exception/SensorException.java
+++ src/main/java/com/sensors/exception/SensorException.java
@@ -1,6 +1,10 @@
 package com.sensors.exception;
 
 public class SensorException extends RuntimeException {
+    public SensorException() {
+        super();
+    }
+
     public SensorException(String message) {
         super(message);
     }
